50e6dbf0efd9400f09b204529d2bca85acd9c8cf
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2017-12-25 18:13:27 -0500

    LeetCode: two-sum problem update

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -25,7 +25,3 @@ if __name__ == '__main__':
     target = -8
     indices = s.twoSum(nums,target)
     print "The indices are:", indices
-ven nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
