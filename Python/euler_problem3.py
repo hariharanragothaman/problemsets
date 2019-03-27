5183db2eccec8c2bb24bd3884a2b3baa3593edac
hariharanragothaman <hariharanragothaman@gmail.com>
2019-03-27 16:29:12 -0400

    Improving the check_prime algorithm

--- Python/euler_problem3.py
+++ Python/euler_problem3.py
@@ -3,42 +3,36 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-test_num = 600851475143
+#test_num = 600851475143
+test_num = 13195
 
-#test_num = 13195
 i = 2
 largest_prime_factor = 0
 factor_limit = test_num / 2
 
-
 def check_prime(num):
-    i = 2
-    flag = True
-    flimit = num/2
-
-    while flimit > i:
-        if num % flimit == 0:
-            flag = False
-            break
-        else:
-            flag=True
-        flimit = flimit - 1
-
-    if flag:
-        print "This factor is a prime number"
-        return num
-    else:
-        print "This factor is not a prime number"
+    if num <=1:
+        return False
+    if num <=3:
+        return True
+    if num%2==0 or num%3==0:
         return False
 
+    i = 5
+    while num >= (i*i):
+        if num%i == 0 or num%(i+2) == 0:
+            return False
+        i += 6
+    return True
+
+# Find all factors
 
 while factor_limit > 2:
     if test_num % factor_limit == 0:
         print "The factor is:", factor_limit
-        print "Checking if the factor is prime..."
         result = check_prime(factor_limit)
         if result:
-            largest_prime_factor = result
+            largest_prime_factor = factor_limit
             break
         else:
             print "Searching for other factors..."
