19b3555be88a52215898d32bd54e7119050fc69d
hariharanragothaman <hariharanragothaman@gmail.com>
2019-04-11 14:30:18 -0400

    Polishing the solution

--- Python/leetcode/ll_7_new.py
+++ Python/leetcode/ll_7_new.py
@@ -2,6 +2,10 @@ import math
 
 def reverse(x):
     limit = math.pow(2, 31)
-    sign = [-1,1][x<0]
+    sign = [1,-1][x < 0]
     ll = sign * int(str(abs(x))[::-1])
-    return ll
+    return ll if -(2**31)-1 < ll < 2**31 else 0
+
+a = 1020300
+rst = reverse(a)
+print rst
