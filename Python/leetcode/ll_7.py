ab36d60d2d9b338713f143fbb9b8ccdaaaa54c9a
hariharanragothaman <hariharanragothaman@gmail.com>
2019-04-11 12:32:02 -0400

    Polishing the reverse function

--- Python/leetcode/ll_7.py
+++ Python/leetcode/ll_7.py
@@ -1,6 +1,10 @@
 # Reverse an integer
 
+import math
+
 def reverse(x):
+
+    limit = math.pow(2, 31)
     ll = [x for x in str(x)]
     str_len = len(ll)
     
@@ -13,33 +17,31 @@ def reverse(x):
         result = ''.join(x for x in ll)
         result = sign + result
 
-    print "The initial result is:", result
+    unsigned  = ''.join(x for x in ll)
 
-
+    if int(unsigned) > limit: return 0
 
     # Converting the result back into a list
     rr = [x for x in result]
     flag = 0
     
     if len(ll) == 1:
-        return rr
+        res = ''.join(x for x in rr)
+        return int(res)
 
-    for i in range(len(ll)):
-        if int(ll[i]) == 0 and int(ll[i+1]) != 0:
-            flag = i+1
-            break
-    print "The value of flag is", flag
+    # Removing all unwanted zero's in the start
+    if int(ll[0]) == 0:
+        for i in range(len(ll)):
+            if int(ll[i]) == 0 and int(ll[i+1]) != 0:
+                flag = i+1
+                break
+
+    # Modifying the string
 
     if not rr[0].isdigit():
-        q = rr[0] + ''.join(rr[flag:])
-        print q
-        return q
+        q = rr[0] + ''.join(rr[flag+1:])
+        return int(q)
     else:
+        print "Entered here"
         q = ''.join(rr[flag:]) 
-        print q
-        return q
-
-
-x = 123 * -1
-result = reverse(x)
-print result
+        return int(q)
