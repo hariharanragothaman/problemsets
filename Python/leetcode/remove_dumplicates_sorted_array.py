259df4548b22a388cd4748e6e03c81a1c548943e
hariharanragothaman <hariharanragothaman@gmail.com>
2019-06-18 17:23:50 -0400

    Updated logic for removing duplicates in an array

--- Python/leetcode/remove_dumplicates_sorted_array.py
+++ Python/leetcode/remove_dumplicates_sorted_array.py
@@ -16,7 +16,34 @@ def removeDuplicates(A):
 
     return A
 
+def remove_duplicates_new(nums):
+
+    """
+    Parse through the array
+    Maintain a slow pointer
+
+    with fast pointer - when elements are not equal
+    move it to the slow pointers next location
+    Increment slow pointer
+
+    A[0---<slow-pointer] is the sub-list we need
+    """
+    j = 1
+    if len(nums) == 0: return 0
+
+
+    print "The initial value of A is", A
+
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            nums[j] = nums[i]
+            j +=1
+            print "The value of A is", A
+
+    del nums[j:len(nums)]
+    return A
+
 A = [0,0,1,1,1,2,2,3,3,4]
 
-rst = removeDuplicates(A)
+rst = remove_duplicates_new(A)
 print rst
