577b0d6f0204af45eb466c254946c0c4027d37b2
Hariharan Ragothaman <hragothaman@Hariharans-MacBook-Air.local>
2019-11-05 20:04:59 -0500

    Adding a test case for this scenario

--- Python/epi/searching/search_for_value_equal_to_index.py
+++ Python/epi/searching/search_for_value_equal_to_index.py
@@ -1,7 +1,7 @@
 def search_entry_equal_to_its_index(A):
     left, right = 0, len(A)-1
     while left <= right:
-        mid = (left + right) / 2
+        mid = (left + right) // 2
         difference = A[mid] - mid
         if difference == 0:
             return mid
@@ -10,3 +10,8 @@ def search_entry_equal_to_its_index(A):
         else:
             left = mid + 1
     return -1
+
+if __name__ == '__main__':
+    A = [-2,0,2,3,6,7,9]  # the array has to be sorted
+    result = search_entry_equal_to_its_index(A)
+    print (result)
