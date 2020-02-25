a885008f400f9f7e196ba42b5a5ab81e9294c0ec
hariharanragothaman <hariharanragothaman@gmail.com>
2020-02-25 12:09:58 -0500

    Solution for finding longest word in dicitonary

--- Python/leetcode/0720_longest_word_in_dictionary.py
+++ Python/leetcode/0720_longest_word_in_dictionary.py
@@ -1,29 +1,34 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        sorted_words = sorted(words, reverse=True) 
+        sorted_words = sorted(words, key=len, reverse=True)        
         print("The sorted words is:", sorted_words)
         
         # Check if substrings are there:
         rlist = []
-        
         for i, item in list(enumerate(sorted_words)):
-            length = len(item)
-        
-            while length > 0:
-                tmp = item[:length-1]
-                if len(tmp) == 1:
-                    rlist.append(item)               
+            length = len(item)            
+            while length > 0:  
+                if length == 1:
+                    tmp = item
+                else:
+                    tmp = item[:length-1]
+                  
                 if tmp not in sorted_words:
                     break
+                if len(tmp) == 1 and tmp in sorted_words and item not in rlist:
+                    rlist.append(item)               
                 length = length -1
-        print(rlist)
         
+        # Result 
         gindex = 0
         for i in range(len(rlist)):
             if len(rlist[i]) > len(rlist[gindex]):
                 gindex = i
-            elif len(rlist[i]) == len(rlist[gindex]):
-                if rlist[i] < rlist[gindex]:
-                    gindex = i 
-        
-        return rlist[gindex]
+                
+        max_length = len(rlist[gindex])      
+        int_list = []
+        for i in range(len(rlist)):
+            if len(rlist[i]) == max_length:
+                int_list.append(rlist[i])
+                
+        return(sorted(int_list)[0])
