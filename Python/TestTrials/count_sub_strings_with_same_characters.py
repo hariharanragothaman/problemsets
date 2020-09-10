00525011c5047f30e44083f3acc090f3aaaf6fc6
hariharanragothaman <hariharanragothaman@gmail.com>
2020-09-10 00:24:37 -0400

    Some minor refactoring and polish

--- Python/TestTrials/count_sub_strings_with_same_characters.py
+++
@@ -1,6 +0,0 @@
-"""
-I/P: abba
-O/P: 5
-a, b,b, a, bb
-number of sub-strings that have the same character.
-"""
--- Python/TestTrials/file-handling.py
+++
@@ -1,8 +0,0 @@
-print("Important Maximum SubArray Problem")
-
-with open("file.txt", "r") as file:
-    #data = file.readlines()
-    #print(data)
-    for line in file:
-        word = line.split()
-        print(word)
